a8938066133fef872be39ded0cc3cda9f3b351d3
Shogo <shogo.baseball0306@gmail.com>
2024-02-17 19:51:52 +0900

    finish making introsection

--- scripts/libs/hero-slider.js
+++ scripts/libs/hero-slider.js
@@ -5,27 +5,29 @@ class HeroSlider {
     }
 
     _initSwiper() {
-        return new Swiper (this.el, {
+        return new Swiper(this.el, {
+            // Optional parameters
+            // direction: 'vertical',
             loop: true,
             grabCursor: true,
             effect: 'coverflow',
             centeredSlides: true,
             slidesPerView: 1,
             speed: 1000,
-            breadpoints: {
+            breakpoints: {
                 1024: {
-                    slidesPerView:2,
+                    slidesPerView: 2,
                 }
             },
-            
-          });
+        });
     }
 
     start(options = {}) {
         options = Object.assign({
             delay: 4000,
-            disableOnInteraction: false,
+            disableOnInteraction: false
         }, options);
+        
         this.swiper.params.autoplay = options;
         this.swiper.autoplay.start();
     }
